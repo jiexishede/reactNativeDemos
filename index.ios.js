690726e3a836b416b8dbf13669d1ae417740b3c5
中文姓名 <lilonglong@lilonglongs-MacBook-Pro.local>
2017-05-07 16:39:40 +0800

    9宫格出来了, 图片没有显示

--- index.ios.js
+++ index.ios.js
@@ -13,6 +13,7 @@ import {
   ListView,
   Image,
   Dimensions,
+  ScrollView,
 } from 'react-native';
 
 const deviceWidth = Dimensions.get('window').width;
@@ -25,170 +26,236 @@ export default class AwesomeProject extends Component {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
     this.state = {
-      dataSource: ds.cloneWithRows([
+       imageArray1: [
+         {
+           url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+           description: 'React Logo'
+         },
+       ],
+      imageArray2: [
         {
-          title: '肺部CT：肿瘤消失',
-          time: '2017-1-21',
-        }, {
-          title: '首次随访：未发现肿瘤',
-          time: '2016-12-1',
-        }, {
-          title: '第二阶段化疗：吉非替尼',
-          time: '2016-9-10~2016-10-10',
-        }, {
-          title: '基因检测：EGFR基因19号外显子突变过长了吗?',
-          time: '2016-8-9',
-        }, {
-          title: '首次化疗：CP方案',
-          time: '2016-6-8~2016-7-7',
-        }, {
-          title: '病理：中低分化腺癌',
-          time: '2016-6-7',
-        }, {
-          title: '左肺大部切除术、肺门淋巴结清扫',
-          time: '2016-6-6',
-        }, {
-          title: '肺部CT：左肺门5cm结节',
-          time: '2016-6-2',
-        }, {
-          title: '初发症状： 咳嗽2周',
-          time: '2016-6-1',
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
         },
-      ])
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+      ],
+      imageArray3: [
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+      ],
+      imageArray4: [
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+
+      ],
+      imageArray6: [
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+
+      ],
+      imageArray11: [
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+        {
+          url: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg',
+          description: 'React Logo'
+        },
+      ],
     };
   }
 
+  renderRow = (tempArray) => {
+
+    const imageStyleNumber = tempArray.length;
+    let imageStyleString;
+    switch (imageStyleNumber) {
+      case 1:
+        imageStyleString = `imgView1`;
+        break;
+      case 2:
+        imageStyleString = `imgView2`;
+        break;
+      case 3:
+        imageStyleString = `imgView3`;
+        break;
+      case 4:
+        imageStyleString = `imgView4`;
+        break;
+      case 5:
+      case 6:
+        imageStyleString = `imgView6`;
+        break;
+      default:
+        imageStyleString = `imgView9`;
+    }
+
+    return tempArray.map((item, index) => {
+      if(index > 8){
+        return ;
+      }
+      return (
+          <View  key={index} style={styles[imageStyleString]}>
+            <Image style={styles.imgIstyle} source={{uri: item.url}}></Image>
+            <Text style={styles.imgTDesc}>{item.description}</Text>
+          </View>
+      )
+    })
+  }
+
   render() {
+    let imageStyleString = '';
+    const imageStyleNumber = this.state.imageArray11.length;
+    switch (imageStyleNumber) {
+      case 1:
+        imageStyleString = `imgView1`;
+        break;
+      case 2:
+        imageStyleString = `imgView2`;
+        break;
+      case 3:
+        imageStyleString = `imgView3`;
+        break;
+      case 4:
+        imageStyleString = `imgView4`;
+        break;
+      case 5:
+      case 6:
+        imageStyleString = `imgView6`;
+        break;
+      default:
+        imageStyleString = `imgView9`;
+    }
     return (
-        <View style={styles.container}>
-          <View style={styles.topView}>
-            <View style={styles.flexTop}>
-              <View style={{
-                flex: 2,
-                alignItems: 'flex-end',
-                flexDirection: 'row',
-              }}>
-                <Image style={{
-                  width: 55,
-                  height: 55,
-                  resizeMode: 'contain',
-                  flex: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center'
-                }} source={require('./image/Oval.png')}>
-                  <Image style={{
-                    width: 48,
-                    height: 48,
-                    resizeMode: 'contain'
-                  }} source={require('./image/Bitmap.png')}/>
-                </Image>
-              </View>
-              <View style={{
-                flex: 7,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignSelf: 'flex-start',
-              }}>
-                <View style={{
-                  flex: 1,
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center'
-                }}>
-                  <Text style={{
-                    flex: 3.6,
-                    fontSize: 14,
-                    fontWeight: "700"
-                  }}>张初华</Text>
-                  <Text style={{
-                    flex: 1,
-                    fontSize: 12.5,
-                    fontWeight: "700"
-                  }}>男</Text>
-                  <Text style={{
-                    flex: 8,
-                    fontSize: 12.5,
-                    fontWeight: "700"
-                  }}>55岁</Text>
-                </View>
-                <Text style={{
-                  flex: 1,
-                  fontSize: 12.5,
-                  fontWeight: "700"
-                }} numberOfLines={1}
-                      ellipsizeMode={'tail'}>右上肺未分化癌 ⅢA期</Text>
-                <Text style={{
-                  flex: 1,
-                  fontSize: 12.5,
-                  fontWeight: "700"
-                }} numberOfLines={1}
-                      ellipsizeMode={'tail'}>简要病史：检查发现颅内占位1+月。</Text>
-              </View>
-            </View>
+        <ScrollView style={styles.container}>
+
+
+          <Image resizeMode="contain" style={{width:70,height:100,backgroundColor:'red'}} source={{uri: 'http://insights.ubuntu.com/wp-content/uploads/8063/react-native-logo.jpg'}}
+          ></Image>
+
+          <View style={styles.rowVImageBox}>
+
+            {this.renderRow(this.state.imageArray1)}
+
           </View>
-          <View style={styles.listViewBox}>
-            <ListView style={{flex:1}} dataSource={this.state.dataSource} renderRow={
-              (rowData, sectionID, rowID, highlightRow) => {
-                if (rowID === "0") {
-                  return (
-                      <View style={styles.listStyle}>
-                        <View style={styles.leftLineView}>
-                          <View>
-                            <Text>看</Text>
-                          </View>
-                        </View>
-                        <View style={styles.rowVContent}>
-                          <View style={styles.rowVContentTop}>
-                            <View style={styles.cellStyle}>
-                              <Text style={
-                                styles.highLightTitleStyle
-                              } numberOfLines={1}
-                                    ellipsizeMode={'tail'}>{rowData.title}</Text>
-                              <Text style={
-                                styles.highLightTimeStyle
-                              }>{rowData.time}</Text>
-                            </View>
-                            <View style={styles.arrowView}>
-                              <Image style={styles.arrowStyle} source={require('./image/arrow.png')}></Image>
-                            </View>
-                          </View>
-                          <View style={styles.cellBottomLine}></View>
-                        </View>
-                      </View>
-                  );
-                } else {
-                  return (
-                      <View style={styles.listStyle}>
-                        <View style={styles.leftLineView}>
-                          <View>
-                            <Text>看</Text>
-                          </View>
-                        </View>
-                        <View style={styles.rowVContent}>
-                          <View style={styles.rowVContentTop}>
-                            <View style={styles.cellStyle}>
-                              <Text style={
-                                styles.titleStyle
-                              } numberOfLines={1}
-                                    ellipsizeMode={'tail'}>{rowData.title}</Text>
-                              <Text
-                                  style={
-                                    styles.timeStyle
-                                  }>{rowData.time}</Text>
-                            </View>
-                            <View style={styles.arrowView}>
-                              <Image style={styles.arrowStyle} source={require('./image/arrow.png')}></Image>
-                            </View>
-                          </View>
-                          <View style={styles.cellBottomLine}></View>
-                        </View>
-                      </View>
-                  );
-                }
-              }
-            }/>
+
+          <View style={styles.rowVImageBox2}>
+
+            {this.renderRow(this.state.imageArray2)}
+            
           </View>
-        </View>
+
+          <View style={styles.rowVImageBox}>
+
+            {this.renderRow(this.state.imageArray3)}
+
+          </View>
+
+          <View style={styles.rowVImageBox2}>
+
+            {this.renderRow(this.state.imageArray4)}
+
+          </View>
+
+          <View style={styles.rowVImageBox}>
+
+            {this.renderRow(this.state.imageArray6)}
+
+          </View>
+
+
+          <View style={styles.rowVImageBox2}>
+
+            {this.renderRow(this.state.imageArray11)}
+          </View>
+          
+        </ScrollView>
     );
   }
 }
@@ -196,89 +263,72 @@ export default class AwesomeProject extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#ebf0f2',
   },
 
-  topView: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    marginTop: 104,
-    width: deviceWidth,
-    height: 76,
-    flexDirection: 'row',
-  },
-  listViewBox: {
-    flex: 7,
-    width:deviceWidth,
-  },
-  flexTop: {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  listStyle: {
+  rowVImageBox: {
     flexDirection:'row',
-    height:59, 
-    backgroundColor:'#ffffff',
+    flexWrap:'wrap',
+    width: 300,
+    height: 300,
+    backgroundColor:'red',
+    marginBottom:100,
   },
-  leftLineView: {
-   flex: 2.5,
+  rowVImageBox2: {
+    flexDirection:'row',
+    flexWrap:'wrap',
+    width: 300,
+    height: 300,
+    marginBottom:100,
+    backgroundColor:'blue',
   },
-  rowVContent: {
-   flex: 16,
-    flexDirection:'column',
-    justifyContent:'space-around',
+  imgView1: {
+    width:290,
+    height:290,
+    marginRight:5,
+    marginBottom:5,
   },
-  rowVContentTop: {
-    flexDirection:'row'
+  imgView2: {
+    width:140,
+    height:290,
+    marginRight:5,
+    marginBottom:5,
   },
-  cellStyle: {
-    flex: 15,
+  imgView3: {
+    width:90,
+    height:290,
+    marginRight:5,
+    marginBottom:5,
   },
-  arrowView: {
-    flex: 1,
-    justifyContent:'center',
+  imgView4: {
+    width:140,
+    height:140,
+    marginRight:5,
+    marginBottom:5,
   },
-  cellBottomLine: {
-    alignItems:'flex-end',
-    backgroundColor: '#DDDDDD',
-    height: 1,
+  imgView6: {
+    width:90,
+    height:140,
+    marginRight:5,
+    marginBottom:5,
   },
-  highLightTitleStyle: {
-    color: '#2bb063',
-    fontSize: 16,
-    flexWrap: 'nowrap',
-    fontWeight: "900",
+  imgView9: {
+    width:90,
+    height:90,
+    marginRight:5,
+    marginBottom:5,
   },
-  highLightTimeStyle: {
-    color: '#2bb063',
-    fontSize: 16,
-    flexWrap: 'nowrap',
-    paddingTop: 1,
+  imgIstyle:{
+    width:'100%',
+    height:'70%',
+    backgroundColor:'yellow',
   },
-
-  titleStyle: {
-    textAlign: 'left',
-    fontWeight: "900",
-    textAlignVertical: 'center',
-    color: '#9e9e9e',
-    fontSize: 16,
-    flexWrap: 'nowrap',
-  },
-  timeStyle: {
-    color: '#a3a3a3',
-    fontSize: 12,
-    fontFamily: 'STHeitiSC-Medium',
-    paddingTop: 1,
-  },
-  arrowStyle: {
-    // backgroundColor:'red',
-    width:6,
-    height:10,
+  imgTDesc: {
+    flex:2,
+    backgroundColor:'white'
   }
+
+
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
