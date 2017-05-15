ec4a33943f8f7c890199fb1cd20706cd9c415ec9
中文姓名 <lilonglong@lilonglongs-MacBook-Pro.local>
2017-05-15 23:42:14 +0800

    webView 传值双向 互传 值可以了

--- index.ios.js
+++ index.ios.js
@@ -14,6 +14,7 @@ import {
   Image,
   WebView,
   Dimensions,
+  TouchableOpacity,
 } from 'react-native';
 
 const {width} = Dimensions.get('window');
@@ -24,22 +25,49 @@ export default class AwesomeProject extends Component {
   constructor(props) {
     super(props);
     const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-   
+    this.state={
+      name:'阿里云',
+      time:'2017-06-04 12:23:22'
+    }
+  }
+
+  webview: WebView
+
+
+  btnClick= ()=> {
+    this.webview.postMessage(JSON.stringify({
+      name:'阿里云',
+      time:'2017-06-04 12:12:33'
+    }));
+  }
+
+  handleMessage = (evt: any) => {
+    const message = JSON.parse(evt.nativeEvent.data)
+
+    if (message.command === 'get info') {
+      const nickname = message.payload.nickname
+      // 得到了数据随你怎么办吧
+    }
   }
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text  style={styles.textSTyle}>Hellow world</Text>
-        {/*<View style={{width:width,height:390, backgroundColor:'red'}}>*/}
+        <View style={styles.container}>
+          <Text  style={styles.textSTyle}>Hellow world</Text>
+          {/*<View style={{width:width,height:390, backgroundColor:'red'}}>*/}
           <WebView
-              source={{uri: 'http://localhost:63342/AwesomeProject/testHtml.html?_ijt=r1o26jv0n7te15n3lbfblsmvhs'}}
+              ref={w => this.webview = w}
+              source={{uri: 'http://localhost:63342/AwesomeProject/testHtml.html?_ijt=kcalhjj5a1bohcr7bvmtsmgcg0'}}
+              onMessage={this.handleMessage}
               style={styles.webStyle}
           />
 
-        {/*</View>*/}
-           <Text  style={styles.textSTyle}>Hellow world</Text>
-      </View>
+          {/*</View>*/}
+          <Text  style={styles.textSTyle}>Hellow world</Text>
+          <TouchableOpacity onPress={this.btnClick}>
+                             <Text style={styles.btnClick}> 点击</Text>
+          </TouchableOpacity>
+        </View>
     );
   }
 }
@@ -60,9 +88,14 @@ const styles = StyleSheet.create({
 
     color:'green',
     backgroundColor:"#fafece"
+  },
+  btnClick: {
+    width:50,
+    height:250,
+    backgroundColor:'pink'
   }
 
-                          
+
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
