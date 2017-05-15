1a16ad08af0b0dd42c8b3b4c0335c06872ea5e27
中文姓名 <lilonglong@lilonglongs-MacBook-Pro.local>
2017-05-15 10:27:33 +0800

    listView 实验做法

--- index.ios.js
+++ index.ios.js
@@ -13,9 +13,48 @@ import {
   ListView,
   Image,
   Dimensions,
+  FlatList,
 } from 'react-native';
 
 const deviceWidth = Dimensions.get('window').width;
+
+
+class  listHeaderView extends Component {
+
+  render(){
+    return (
+        <View style={{width:'100%',height:60, backgroundColor:'brown'}}>
+          <Text style={{color:'white',fontSize:18}}>Header</Text>
+        </View>
+    )
+
+  }
+}
+
+
+class  listFooterView extends Component {
+
+  render(){
+    return (
+        <View style={{width:'100%',height:40, backgroundColor:'red'}}>
+          <Text style={{color:'white',fontSize:18}}>Footer</Text>
+        </View>
+    )
+
+  }
+}
+
+class  seprateLineView extends  Component {
+
+  render() {
+    return (
+        <View style={{height:2,backgroundColor:'#adadad'}}/>
+    )
+
+  }
+}
+
+
 
 
 export default class AwesomeProject extends Component {
@@ -61,132 +100,15 @@ export default class AwesomeProject extends Component {
   render() {
     return (
         <View style={styles.container}>
-          <View style={styles.topView}>
-            <View style={styles.flexTop}>
-              <View style={{
-                flex: 2,
-                alignItems: 'flex-end',
-                flexDirection: 'row',
-              }}>
-                <Image style={{
-                  width: 55,
-                  height: 55,
-                  resizeMode: 'contain',
-                  flex: 1,
-                  justifyContent: 'center',
-                  alignItems: 'center'
-                }} source={require('./image/Oval.png')}>
-                  <Image style={{
-                    width: 48,
-                    height: 48,
-                    resizeMode: 'contain'
-                  }} source={require('./image/Bitmap.png')}/>
-                </Image>
-              </View>
-              <View style={{
-                flex: 7,
-                flexDirection: 'column',
-                justifyContent: 'center',
-                alignSelf: 'flex-start',
-              }}>
-                <View style={{
-                  flex: 1,
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center'
-                }}>
-                  <Text style={{
-                    flex: 3.6,
-                    fontSize: 14,
-                    fontWeight: "700"
-                  }}>张初华</Text>
-                  <Text style={{
-                    flex: 1,
-                    fontSize: 12.5,
-                    fontWeight: "700"
-                  }}>男</Text>
-                  <Text style={{
-                    flex: 8,
-                    fontSize: 12.5,
-                    fontWeight: "700"
-                  }}>55岁</Text>
-                </View>
-                <Text style={{
-                  flex: 1,
-                  fontSize: 12.5,
-                  fontWeight: "700"
-                }} numberOfLines={1}
-                      ellipsizeMode={'tail'}>右上肺未分化癌 ⅢA期</Text>
-                <Text style={{
-                  flex: 1,
-                  fontSize: 12.5,
-                  fontWeight: "700"
-                }} numberOfLines={1}
-                      ellipsizeMode={'tail'}>简要病史：检查发现颅内占位1+月。</Text>
-              </View>
-            </View>
-          </View>
-          <View style={styles.listViewBox}>
-            <ListView style={{flex:1}} dataSource={this.state.dataSource} renderRow={
-              (rowData, sectionID, rowID, highlightRow) => {
-                if (rowID === "0") {
-                  return (
-                      <View style={styles.listStyle}>
-                        <View style={styles.leftLineView}>
-                          <View>
-                            <Text>看</Text>
-                          </View>
-                        </View>
-                        <View style={styles.rowVContent}>
-                          <View style={styles.rowVContentTop}>
-                            <View style={styles.cellStyle}>
-                              <Text style={
-                                styles.highLightTitleStyle
-                              } numberOfLines={1}
-                                    ellipsizeMode={'tail'}>{rowData.title}</Text>
-                              <Text style={
-                                styles.highLightTimeStyle
-                              }>{rowData.time}</Text>
-                            </View>
-                            <View style={styles.arrowView}>
-                              <Image style={styles.arrowStyle} source={require('./image/arrow.png')}></Image>
-                            </View>
-                          </View>
-                          <View style={styles.cellBottomLine}></View>
-                        </View>
-                      </View>
-                  );
-                } else {
-                  return (
-                      <View style={styles.listStyle}>
-                        <View style={styles.leftLineView}>
-                          <View>
-                            <Text>看</Text>
-                          </View>
-                        </View>
-                        <View style={styles.rowVContent}>
-                          <View style={styles.rowVContentTop}>
-                            <View style={styles.cellStyle}>
-                              <Text style={
-                                styles.titleStyle
-                              } numberOfLines={1}
-                                    ellipsizeMode={'tail'}>{rowData.title}</Text>
-                              <Text
-                                  style={
-                                    styles.timeStyle
-                                  }>{rowData.time}</Text>
-                            </View>
-                            <View style={styles.arrowView}>
-                              <Image style={styles.arrowStyle} source={require('./image/arrow.png')}></Image>
-                            </View>
-                          </View>
-                          <View style={styles.cellBottomLine}></View>
-                        </View>
-                      </View>
-                  );
-                }
-              }
-            }/>
+          <View style={styles.listBox}>
+            <FlatList style={styles.listView}
+                      ListHeaderComponent={listHeaderView}
+                      ListFooterComponent={listFooterView}
+                      ItemSeparatorComponent={seprateLineView}
+                      extraData = { {headerText :'extraHeader', footerText:'extraFooter'}}
+                data={[{key: 'a'}, {key: 'b'},{key: 'c'}, {key: 'd'}]}
+                renderItem={({item,index}) => <Text style={[styles.itemTStyle,(index % 2) ? {  backgroundColor : 'blue'} : {backgroundColor:'white'}] }>{item.key}</Text>}
+            />
           </View>
         </View>
     );
@@ -198,87 +120,25 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    flexDirection:'row-reverse',
     backgroundColor: '#ebf0f2',
   },
-
-  topView: {
-    flex: 1,
-    backgroundColor: '#ffffff',
-    marginTop: 104,
-    width: deviceWidth,
-    height: 76,
-    flexDirection: 'row',
-  },
-  listViewBox: {
-    flex: 7,
-    width:deviceWidth,
-  },
-  flexTop: {
-    flex: 1,
-    alignItems: 'center',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-  },
-  listStyle: {
-    flexDirection:'row',
-    height:59, 
-    backgroundColor:'#ffffff',
-  },
-  leftLineView: {
-   flex: 2.5,
-  },
-  rowVContent: {
-   flex: 16,
-    flexDirection:'column',
-    justifyContent:'space-around',
-  },
-  rowVContentTop: {
-    flexDirection:'row'
-  },
-  cellStyle: {
-    flex: 15,
-  },
-  arrowView: {
-    flex: 1,
+  listBox: {
+    width:'50%',
+    height:'50%',
+    backgroundColor:'purple',
     justifyContent:'center',
+    alignItems:'flex-start', 
   },
-  cellBottomLine: {
-    alignItems:'flex-end',
-    backgroundColor: '#DDDDDD',
-    height: 1,
+  listView: {
+    backgroundColor:'green',
+    width:'100%',
   },
-  highLightTitleStyle: {
-    color: '#2bb063',
-    fontSize: 16,
-    flexWrap: 'nowrap',
-    fontWeight: "900",
-  },
-  highLightTimeStyle: {
-    color: '#2bb063',
-    fontSize: 16,
-    flexWrap: 'nowrap',
-    paddingTop: 1,
-  },
-
-  titleStyle: {
-    textAlign: 'left',
-    fontWeight: "900",
-    textAlignVertical: 'center',
-    color: '#9e9e9e',
-    fontSize: 16,
-    flexWrap: 'nowrap',
-  },
-  timeStyle: {
-    color: '#a3a3a3',
-    fontSize: 12,
-    fontFamily: 'STHeitiSC-Medium',
-    paddingTop: 1,
-  },
-  arrowStyle: {
-    // backgroundColor:'red',
-    width:6,
-    height:10,
+  itemTStyle: {
+    height:30,
+    textAlign:"center",
   }
+
 });
 
 AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
